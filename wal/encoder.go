63a9a3e06bd31b5ec5390a5144b93371e95e5dd7
Athlum <athlum5211@outlook.com>
2018-06-07 17:55:31 +0800

    test

--- wal/encoder.go
+++ wal/encoder.go
@@ -35,15 +35,15 @@ type encoder struct {
 	mu sync.Mutex
 	bw *ioutil.PageWriter
 
-	crc       hash.Hash32
+	prevCrc   uint32
 	buf       []byte
 	uint64buf []byte
 }
 
 func newEncoder(w io.Writer, prevCrc uint32, pageOffset int) *encoder {
 	return &encoder{
-		bw:  ioutil.NewPageWriter(w, walPageBytes, pageOffset),
-		crc: crc.New(prevCrc, crcTable),
+		bw:      ioutil.NewPageWriter(w, walPageBytes, pageOffset),
+		prevCrc: prevCrc,
 		// 1MB buffer
 		buf:       make([]byte, 1024*1024),
 		uint64buf: make([]byte, 8),
@@ -60,31 +60,23 @@ func newFileEncoder(f *os.File, prevCrc uint32) (*encoder, error) {
 }
 
 func (e *encoder) encode(rec *walpb.Record) error {
-	e.mu.Lock()
-	defer e.mu.Unlock()
-
-	e.crc.Write(rec.Data)
-	rec.Crc = e.crc.Sum32()
+	c := crc.New(e.prevCrc, crcTable)
+	c.Write(rec.Data)
+	rec.Crc = c.Sum32()
 	var (
 		data []byte
 		err  error
 		n    int
 	)
 
-	if rec.Size() > len(e.buf) {
-		data, err = rec.Marshal()
-		if err != nil {
-			return err
-		}
-	} else {
-		n, err = rec.MarshalTo(e.buf)
-		if err != nil {
-			return err
-		}
-		data = e.buf[:n]
+	data, err = rec.Marshal()
+	if err != nil {
+		return err
 	}
-
 	lenField, padBytes := encodeFrameSize(len(data))
+
+	e.mu.Lock()
+	defer e.mu.Unlock()
 	if err = writeUint64(e.bw, lenField, e.uint64buf); err != nil {
 		return err
 	}
